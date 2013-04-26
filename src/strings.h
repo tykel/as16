2a76ac9b118fb43fd1c2e9d2450a970bb338b296
Tim Kelsall <timothy.kelsall@gmail.com>
2013-04-26 23:01:01 +0100

    Added strings from mash16 disassembler

---
+++ src/strings.h
@@ -0,0 +1,26 @@
+/*
+ *   as16 - the chip16 emulator
+ *   Copied partly from mash16
+ *   Copyright (C) 2012-2013 tykel
+ *
+ *   as16 is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   as16 is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with as16.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef STRINGS_
+#define STRINGS_H
+
+extern const char* const str_ops[];
+extern const char* const str_cond[];
+
+#endif
